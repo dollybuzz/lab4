9e16d4a58708d303dd603ea01cb08c274eae0b2d
Dalia <43527456+dollybuzz@users.noreply.github.com>
2019-11-17 08:37:40 +0000

    added more routes

--- app.js
+++ app.js
@@ -21,6 +21,16 @@ app.get("/earth", function(req, res) {
     res.render("earth.html");
 });
 
+app.get("/mars", function(req, res) {
+    res.render("mars.html");
+});
+
+app.get("/jupiter", function(req, res) {
+    res.render("jupiter.html");
+});
+
+
+
 //server listener
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Running Express Server...");
