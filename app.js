f95050b8eb20406b90acdc0aa395d3ca01ce9a75
Dalia <43527456+dollybuzz@users.noreply.github.com>
2019-11-17 07:26:21 +0000

    adding procfile and .gitignore file to prevent node_modules from pushing, package.json ensures node modules dependency versioning

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.get("/venus", function(req, res) {
 })
 
 //server listener
-app.listen("8080", "127.0.0.1", function() {
-    console.log("Express server is Running...");
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Running Express Server...");
 });
+
 
