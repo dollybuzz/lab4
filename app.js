9dda24472db04294c5c8f54473d72a83d0dfdb8f
Dalia <43527456+dollybuzz@users.noreply.github.com>
2019-11-17 04:07:39 +0000

    on step 5

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const app = express();
+app.engine('html', require('ejs').renderFile);
 
 //routes
 app.get("/", function(req, res) {
-    res.send("it works!");
+    res.render("index.html");
 });
 
 app.get("/mercury", function(req, res) {
-    res.send("This will be Mercury web page!");
+    res.render("mercury.html");
 });
 
 app.get("/venus", function(req, res) {
-    res.send("This will be Venus web page!");
+    res.render("venus.html");
 })
 
 //server listener
